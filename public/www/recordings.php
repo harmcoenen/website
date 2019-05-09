9c03da1eb8b064d8120aa3c4cb1936c145512465
harmcoenen <32400944+harmcoenen@users.noreply.github.com>
2019-05-09 16:54:12 +0200

    w3.css in recordings.php

--- public/www/recordings.php
+++ public/www/recordings.php
@@ -10,7 +10,6 @@
     <meta name="author" content="Harm Coenen" />
     <meta name="robots" content="index,follow" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="recordings.css">
     <link rel="stylesheet" href="w3-custom.css">
     <link rel="stylesheet" href="w3.css">
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.jpg" />
@@ -49,8 +48,9 @@ function humanReadable($bytes, $decimals = 2) {
 
 function printPath() {
         $path = getcwd();
-        print "<div class=\"w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4\">";
-        print "Path: " . $path . "</div>";
+        print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+        print "Path: $path";
+        print "</div>";
 }
 
 function getFilter() {
@@ -92,56 +92,40 @@ function makeThumbnail($src, $dest, $desired_width) {
     imagedestroy($virtual_image);
 }
 
-function toggleBackGroundColor($bg_color) {
-    if ($bg_color == "#cc99ff") {
-        $bg_color = "#ccccff";
-    } else {
-        $bg_color = "#cc99ff";
-    }
-    return $bg_color;
-}
-
 function presentTimeslots($dir) {
     if (is_dir($dir)) {
         chdir($dir);
         printPath();
 
         if ($timeslots = glob("*", GLOB_ONLYDIR)) {
-            $coloms = 6;
             $slots_per_period = 24;
-            $bg_color = "#ccccff";
             rsort($timeslots);
             $n_timeslots = count($timeslots);
 
-            //print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
-            print "<div class='w3-container w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
-            //print "<table class=\"w3-table-all\">";
-            print "<table style='width:100%'>";
-            print "<th colspan='$coloms'>$n_timeslots timeslots found</th>";
-            //echo("<center><table style='width:90%'>");
-            //echo("<th colspan=\"$coloms\">$n_timeslots timeslots found</th>");
+            print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+            print "$n_timeslots timeslots found";
+            print "</div>";
 
             for($index = 0; $index < $n_timeslots; $index++) {
                 $timeslot = $timeslots[$index];
-                if (($index % $slots_per_period) == 0) $bg_color = toggleBackGroundColor($bg_color);
-                //if (($index % $coloms) == 0) echo("<tr bgcolor=$bg_color>");
-                if (($index % $coloms) == 0) print "<tr bgcolor=$bg_color>";
-                //echo("<td><button onclick=\"location.href='" . RECORDINGS_PHP_URL . "?timeslot=$dir/$timeslot'\" class=\"button\">$timeslot</button></td>");
-                print "<td><a class='w3-center w3-btn w3-border w3-border-purple w3-round-xxlarge' href='" . RECORDINGS_PHP_URL . "?timeslot=$dir/$timeslot'>$timeslot</a></td>";
-                //if ((($index + 1) % $coloms) == 0) echo("</tr>");
-                if ((($index + 1) % $coloms) == 0) print "</tr>";
+                if ($index == 0) {
+                    print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+                } else {
+                    if (($index % $slots_per_period) == 0) {
+                        print "</div>";
+                        print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+                    }
+                }
+                print "<a class='w3-center w3-btn w3-border w3-border-purple w3-margin-bottom w3-margin-right w3-round-xxlarge' href='" . RECORDINGS_PHP_URL . "?timeslot=$dir/$timeslot'>$timeslot</a>";
             }
-            print "</table>";
             print "</div>";
         } else {
-            //echo("<center><h1>No timeslots found</h1></center>");
-            print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+            print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-red w3-margin-top w3-margin-bottom w3-padding-4'>";
             print "<h1>No timeslots found</h1>";
             print "</div>";
         }
     } else {
-        //echo("<center><h1>[$dir] is not a directory</h1></center>");
-        print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+        print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-red w3-margin-top w3-margin-bottom w3-padding-4'>";
         print "<h1>[$dir] is not a directory</h1>";
         print "</div>";
     }
@@ -158,34 +142,49 @@ function presentPictures($timeslot) {
             $totalsize = 0;
             rsort($pictures);
             $n_pictures = count($pictures);
-            echo("<center><table style='width:90%'>");
             for($index = 0; $index < $n_pictures; $index++) {
                 $totalsize += filesize($pictures[$index]);
             }
             $totalsize = humanReadable($totalsize);
-            echo("<th colspan=\"$coloms\">$n_pictures pictures found (total size $totalsize)</th>");
+
+            print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+            print "$n_pictures pictures found (total size $totalsize)";
+            print "</div>";
+
             for($index = 0; $index < $n_pictures; $index++) {
                 $picture = $pictures[$index];
-                $picturesize = humanReadable(filesize($picture));
+                $caption = rtrim($picture, ".jpeg");
+                $caption = rtrim($caption, "_py");
+                //$picturesize = humanReadable(filesize($picture));
                 // Get original size information from picture
-                list($org_width, $org_height, $type, $attr) = getimagesize($picture);
+                //list($org_width, $org_height, $type, $attr) = getimagesize($picture);
                 // Divide original size by 16 to get small thumbnail icons
                 // 1280x720 (divide by 16 = 80x45) or 1600x1200 (divide by 16 = 100x75)
-                $tn_width = floor($org_width / 16);
-                $tn_height = floor($org_height / 16);
-                
-                if (($index % $coloms) == 0) echo("<tr>");
-                //echo("<td><button onclick=\"location.href='" . RECORDINGS_PHP_URL . "?picture=$timeslot/$picture&$filter'\" class=\"button\">$picture<br>($picturesize)</button></td>");
-                //echo("<td><button onclick=\"location.href='" . RECORDINGS_PHP_URL . "?picture=$timeslot/$picture&$filter'\" class=\"button\">$picture<br><img src=$timeslot/$picture width=$tn_width height=$tn_height alt=\"not found\"></img><br>($picturesize)<br>($tn_width x $tn_height), ($org_width x $org_height)</button></td>");
-                echo("<td><button onclick=\"location.href='" . RECORDINGS_PHP_URL . "?picture=$timeslot/$picture&$filter'\" class=\"button\">$picture<br><br><img src=$timeslot/$picture width=$tn_width height=$tn_height alt=\"not found\"></img><br>($picturesize)</button></td>");
-                if ((($index + 1) % $coloms) == 0) echo("</tr>");
+                //$tn_width = floor($org_width / 16);
+                //$tn_height = floor($org_height / 16);
+                if ($index == 0) {
+                    print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+                } else {
+                    if (($index % $coloms) == 0) {
+                        print "</div>";
+                        print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+                    }
+                }
+                print "<a class='w3-center w3-btn w3-border w3-border-purple w3-margin-bottom w3-margin-right w3-round-xxlarge' href='" . RECORDINGS_PHP_URL . "?picture=$timeslot/$picture&$filter'>$caption<br>";
+                //print "<img src=$timeslot/$picture width=$tn_width height=$tn_height alt='loading...'>";
+                print "<img src=$timeslot/$picture height=100 alt='loading...'>";
+                print "</a>";
             }
-            echo("</table></center>");
+            print "</div>";
         } else {
-            echo("<center><h1>No pictures found</h1></center>");
+            print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-red w3-margin-top w3-margin-bottom w3-padding-4'>";
+            print "<h1>No pictures found</h1>";
+            print "</div>";
         }
     } else {
-        echo("<center><h1>[$timeslot] is not a directory</h1></center>");
+        print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-red w3-margin-top w3-margin-bottom w3-padding-4'>";
+        print "<h1>[$timeslot] is not a directory</h1>";
+        print "</div>";
     }
 }
 
@@ -205,53 +204,60 @@ function presentPicture($picture) {
                 $prev_index = $index - 1;
                 $next_index = $index + 1;
                 $position = $index + 1; // array[0] is position 1
-                echo("<center>This picture is found in position [$position/$n_pictures]</center><br>");
-                echo("<center><table style='width:100%; border:0px'><tr>");
+                print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+                print "This picture is found in position [$position/$n_pictures]";
+                print "</div>";
+
+                print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
                 if ($prev_index >= 0) {
                     $prev_file = $pictures[$prev_index];
-                    echo("<td style='width:50%; border:0px' align=\"left\"><button onclick=\"location.href='" . RECORDINGS_PHP_URL . "?picture=$picture_base/$prev_file&$filter'\" class=\"button\">LATER</button></td>");
-                } else {
-                    echo("<td style='width:50%; border:0px'></td>");
+                    print "<a class='w3-margin-left w3-margin-right w3-left w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "?picture=$picture_base/$prev_file&$filter'>LATER</a>";
                 }
                 if ($next_index < $n_pictures) {
                     $next_file = $pictures[$next_index];
-                    echo("<td style='width:50%; border:0px' align=\"right\"><button onclick=\"location.href='" . RECORDINGS_PHP_URL . "?picture=$picture_base/$next_file&$filter'\" class=\"button\">EARLIER</button></td>");
-                } else {
-                    echo("<td style='width:50%; border:0px'></td>");
+                    print "<a class='w3-margin-left w3-margin-right w3-right w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "?picture=$picture_base/$next_file&$filter'>EARLIER</a>";
                 }
-                echo("</tr></table></center>");
+                print "</div>";
             } else {
-                echo("<center><h1>No more pictures found</h1></center>");
+                print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-red w3-margin-top w3-margin-bottom w3-padding-4'>";
+                print "<h1>No more pictures found</h1>";
+                print "</div>";
             }
-            echo("<center><img src='$picture'></img></center><br>");
-            echo("<center>Base is [$picture_base], File is [$picture_file]</center><br>");
-            echo("<center>Picture fullname is [$picture]</center><br>");
+            print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
+            print "<img src='$picture' alt='loading...'><br>";
+            print "<br>Base is [$picture_base], File is [$picture_file]";
+            print "<br>Picture fullname is [$picture]";
+            print "</div>";
         } else {
-            echo("<center><h1>[$picture_base] is not a directory</h1></center>");
+            print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-red w3-margin-top w3-margin-bottom w3-padding-4'>";
+            print "<h1>[$picture_base] is not a directory</h1>";
+            print "</div>";
         }
     } else {
-        echo("<center><h1>[$picture] is not a regular file</h1></center>");
+        print "<div class='w3-container w3-card w3-center w3-pale-purple w3-text-red w3-margin-top w3-margin-bottom w3-padding-4'>";
+        print "<h1>[$picture] is not a regular file</h1>";
+        print "</div>";
     }
 }
 
 /* Main program */
 $starttime = microtime(true);
 
-print "<header class=\"w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4\">";
+print "<header class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>";
 if (isset($_GET['picture'])) {
     $refesh_page = getBase($_GET["picture"]);
     $filter = getFilter();
-    print "<a class=\"w3-margin-left w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large\" href='" . RECORDINGS_PHP_URL . "'>Overview</a>";
-    print "<a class=\"w3-margin-left w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large\" href='" . RECORDINGS_PHP_URL . "?timeslot=$refesh_page&$filter'>Step back</a>";
+    print "<a class='w3-margin-bottom w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "'>Overview</a>";
+    print "<a class='w3-margin-bottom w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "?timeslot=$refesh_page&$filter'>Step back</a>";
 } else if (isset($_GET['timeslot'])) {
     $refesh_page = $_GET["timeslot"];
-    print "<a class=\"w3-margin-left w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large\" href='" . RECORDINGS_PHP_URL . "'>Overview</a>";
-    print "<a class=\"w3-margin-left w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large\" href='" . RECORDINGS_PHP_URL . "?timeslot=$refesh_page'>All</a>";
-    print "<a class=\"w3-margin-left w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large\" href='" . RECORDINGS_PHP_URL . "?timeslot=$refesh_page&picam'>PIcam</a>";
-    print "<a class=\"w3-margin-left w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large\" href='" . RECORDINGS_PHP_URL . "?timeslot=$refesh_page&ipcam'>IPcam</a>";
+    print "<a class='w3-margin-bottom w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "'>Overview</a>";
+    print "<a class='w3-margin-bottom w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "?timeslot=$refesh_page'>All</a>";
+    print "<a class='w3-margin-bottom w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "?timeslot=$refesh_page&picam'>PIcam</a>";
+    print "<a class='w3-margin-bottom w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "?timeslot=$refesh_page&ipcam'>IPcam</a>";
 } else {
-    print "<a class=\"w3-margin-left w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large\" href='" . MAIN_URL . "'>www.familiecoenen.nl</a>";
-    print "<a class=\"w3-margin-left w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large\" href='" . RECORDINGS_PHP_URL . "'>Refresh</a>";
+    print "<a class='w3-margin-bottom w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large' href='" . MAIN_URL . "'>www.familiecoenen.nl</a>";
+    print "<a class='w3-margin-bottom w3-margin-right w3-center w3-btn w3-border w3-border-purple w3-round-large' href='" . RECORDINGS_PHP_URL . "'>Refresh</a>";
 }
 print "</header>";
 
@@ -265,7 +271,7 @@ if (isset($_GET['picture'])) {
 
 $stoptime = microtime(true);
 
-print "<footer class=\"w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4\">| Execution time: " . number_format( ($stoptime - $starttime), 5) . " |</footer>";
+print "<footer class='w3-container w3-card w3-center w3-pale-purple w3-text-purple w3-margin-top w3-margin-bottom w3-padding-4'>| Execution time: " . number_format( ($stoptime - $starttime), 5) . " |</footer>";
 
 ?>
 

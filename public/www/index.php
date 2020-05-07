7dbcc79007ec68902c65458c7c30e800c44daf59
Harm Coenen <harm@familiecoenen.nl>
2020-05-07 20:20:07 +0200

    Update web links in group 'Algemeen'

--- public/www/index.php
+++ public/www/index.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="w3.css">
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.jpg" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body class="w3-pale-purple">
